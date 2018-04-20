c303d4f7bf74371aecdded6987b18c916d231861
Sergei Nosov <sergeinosov6@icloud.com>
2018-04-20 22:35:46 +0300

    Update prefs

---
+++ HistoryARFinal/Assets/scripts/SpawnObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SpawnObjects : MonoBehaviour {
+
+    public GameObject spawnpoint;
+    public GameObject spawnButton;
+    public GameObject RestartButton;
+    public GameObject obj1;
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+    public void SpawnObj()
+    {
+        GameObject ob1 = Instantiate(obj1, transform.localPosition, Quaternion.identity) as GameObject;
+        ob1.transform.position = spawnpoint.transform.position;
+        Destroy(spawnpoint);
+        spawnButton.SetActive(false);
+        RestartButton.SetActive(true);
+    }
+    public void RestartLvl()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
